57d40c6840f8aa3f91e431cfbe8449394cae43d3
Kazım Birol Çağlar <33399079+birolcaglar97@users.noreply.github.com>
2023-07-14 11:10:59 +0300

    Update BaseSteps.java

--- BaseSteps.java
+++ BaseSteps.java
@@ -1,8 +1,28 @@
+import java.util.List;
 
 public class BaseSteps {
-    void bug(){
-        for (int i = 2; i < 0; i++) {
-            System.out.println("Teknasyon");
+    static void bug(){
+        for (int i = 0; i > 0; i++) {
+            System.out.println("birol");
         }
+    }
+    static void bug2(){
+        for (int i = 1; i < 0; i++) {
+            System.out.println("birol");
+        }
+    }
+
+    static void bug3(){
+        List<String> list;
+        list.add("Birol");
+        for (int i = 1; i < 0; i++) {
+            list.get(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        bug();
+        bug2();
+        bug3();
     }
 }
